2467716214d716dd3c2346439cd6706ab74f0750
Itisa <shshr2005@163.com>
2025-02-16 20:34:40 +0800

    fix

--- llmsite/urls.py
+++ llmsite/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
     path("site/", include("mainsite.urls")),
     path("admin/", admin.site.urls),
 ]
