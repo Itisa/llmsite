bee7d1e2b4a48b2ba046cdf0306872682d740576
Itisa <shshr2005@163.com>
2025-03-22 16:52:14 +0800

    fix set cache port to default

--- llmsite/settings.py
+++ llmsite/settings.py
@@ -147,6 +147,6 @@ LOGGING = {
 CACHES = {
 	"default": {
 		"BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
-		"LOCATION": "127.0.0.1:14273",
+		"LOCATION": "127.0.0.1:11211",
 	}
 }
